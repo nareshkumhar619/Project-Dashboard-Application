e72e830233901afb5962cee302ddfe7a6c2cfbc5
Naresh Kumhar <122338149+nareshkumhar619@users.noreply.github.com>
2024-02-23 03:20:28 +0530

    backend commit

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { adminLogin, adminRegister } from '../../controller/admincontroller.js';
+import { adminLogin, adminRegister } from '../../controller/adminController.js';
 
 
 const router = express.Router()
